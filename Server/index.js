00c61b32bcf2568ba629db962bcffe3e8fdae7b6
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-10-08 19:30:10 +0200

    basic iteration of chat done with broadcast function

---
+++ Server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+// cors middleware
+app.use(cors());
+
+// express syntax for creating a server:
+const server = http.createServer(app);
+
+// setup frontend origin and specify methods that we are accepting
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+// tell our server to start listening for some events
+io.on("connection", (socket) => {
+    // we log this just to check that the connection is working
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    // check we are reciving data: 
+    console.log(data)
+
+    // emit all messages received back to the cleints:
+    socket.broadcast.emit("receive_message", data)
+
+
+  });
+});
+
+// specify which port it is listening to
+server.listen(5174, () => {
+  console.log("SERVER IS RUNNING");
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from 'react'
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:5174");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const sendMessage = () => {
+    socket.emit('send_message', {message:'hello'})
+
+  };
+
+  useEffect (() => {
+    socket.on('receive_message', (data) => {
+      alert(data.message)
+    })
+  }, [socket])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input placeholder="Message..." />
+      <button onClick={sendMessage}> Send Message</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
