85151d970a141ff8b4ed6fd5c1315644acb2ef24
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-10-15 17:25:07 +0200

    message sent and receive states updated

--- Server/index.js
+++ Server/index.js
@@ -13,24 +13,22 @@ const server = http.createServer(app);
 // setup frontend origin and specify methods that we are accepting
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
 // tell our server to start listening for some events
 io.on("connection", (socket) => {
-    // we log this just to check that the connection is working
+  // we log this just to check that the connection is working
   console.log(`User connected: ${socket.id}`);
 
   socket.on("send_message", (data) => {
-    // check we are reciving data: 
-    console.log(data)
+    // check we are reciving data:
+    console.log(data);
 
-    // emit all messages received back to the cleints:
-    socket.broadcast.emit("receive_message", data)
-
-
+    // emit all messages received back to the clients:
+    socket.broadcast.emit("receive_message", data);
   });
 });
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,34 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:5174");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  
   const sendMessage = () => {
-    socket.emit('send_message', {message:'hello'})
-
+    socket.emit("send_message", { message });
   };
 
-  useEffect (() => {
-    socket.on('receive_message', (data) => {
-      alert(data.message)
-    })
-  }, [socket])
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
 
   return (
     <div className="App">
-      <input placeholder="Message..." />
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
       <button onClick={sendMessage}> Send Message</button>
+      <h1>message received:</h1>
+      {messageReceived}
     </div>
   );
 }
